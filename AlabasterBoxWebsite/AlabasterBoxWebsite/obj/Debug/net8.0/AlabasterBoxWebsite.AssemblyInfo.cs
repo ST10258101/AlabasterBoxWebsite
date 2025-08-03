50efa4a6c40fc879c639059b895e77caf250d194
ST10258101 <130571344+ST10258101@users.noreply.github.com>
2025-08-03 18:33:13 +0200

    ui login fixes
    the ui has been edited to look better with the npos logo and our groups logo has been added to the about us page

--- AlabasterBoxWebsite/AlabasterBoxWebsite/obj/Debug/net8.0/AlabasterBoxWebsite.AssemblyInfo.cs
+++ AlabasterBoxWebsite/AlabasterBoxWebsite/obj/Debug/net8.0/AlabasterBoxWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlabasterBoxWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a9bfde807e91806e21d7068f7c2f58d43004a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861c0f358ee047a21751618429bf3ca54ebe646b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlabasterBoxWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlabasterBoxWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
